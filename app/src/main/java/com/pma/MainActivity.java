e035631b9c7113eb9890356086a26074034070f6
Tano Dzhinski <tano.dzhinski@gmail.com>
2018-02-05 12:22:44 +0200

    Use Kotlin for data classes.

--- app/src/main/java/com/pma/MainActivity.java
+++ app/src/main/java/com/pma/MainActivity.java
@@ -15,16 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setData(new MainViewModel());
     }
